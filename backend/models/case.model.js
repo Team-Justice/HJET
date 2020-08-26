57e743ec29f73c75cf233cea1f633a0cacf0e9da
Ermelinda Izihirwe <eizihirwe@gmail.com>
2020-08-26 09:14:29 -0400

    backend set up (multiple commits) (#33)
    * set up server and connected it tomongodb
    
    * adding schema for cases
    
    * setting up router for cases

---
+++ backend/models/case.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const caseSchema = new Schema({
+    _caseId: {type: Schema.Types.ObjectId, required:true},
+    firstname: {type: String, required: true, trim:true},
+    lastname: {type: String, required: true, trim:true},
+    ethinicity: {type: String, required: true},
+    gender: {type: String, required: true},
+    address: {type: String, required: true, trim: true},
+    phonenumber: {type: Number, required: true},
+    homedescription: {type: String, required: true, trim: true},
+    own: {type: Boolean, required: true},
+    rent: {type: Boolean, required: true},
+    residencystartdate: {type: Date, required: true}, //use to compute length of time in home
+    estimatedvalue: {type: Number, required: true, trim: true, min: 0},
+    ageofhome: {type: Number, required: true, trim: true, min: 0},
+    householdincome: {type: Number, required: true, trim: true, min: 0},
+    numberofresidents: {
+        adults: {type: Number, required: true, trim: true, min: 0},
+        children: {type: Number, required: true, trim: true, min: 0},
+    },
+    bedrooms: {type: Number, required: true, trim: true, min: 0},
+    baths: {type: Number, required: true, trim: true, min: 0},
+    squarefootage: {type: Number, required: true, trim: true, min: 0},
+    recentlyrenovated: {type: Boolean, required: true},
+    needsrenovation: {type: Boolean, required: true},
+    previoushomeowner: {type: Boolean, required: true},
+    veteran: {type: Boolean, required: true},
+    accomodations: {type: String, required: true, trim: true},
+}, { timestamps: true });
+
+const Case = mongoose.model('Case', caseSchema);
+
+module.exports = Case;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB connected successfully!");
+});
+
+// Routes
+const casesRouter = require('./routes/cases');
+
+app.use('/cases', casesRouter);
+
+// Server
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
