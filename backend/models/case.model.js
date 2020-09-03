461089ff4ce2c196325ea8e09822885455a269c8
Ermelinda Izihirwe <eizihirwe@gmail.com>
2020-09-03 13:54:27 -0400

    setting up edit case page

--- backend/models/case.model.js
+++ backend/models/case.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const caseSchema = new Schema({
+<<<<<<< HEAD
     //_caseId: {type: Schema.Types.ObjectId, required:true},
     firstName: {type: String, required: true, trim:true},
     lastName: {type: String, required: true, trim:true},
@@ -31,6 +32,35 @@ const caseSchema = new Schema({
     recentlyRenovated: {type: Boolean, required: true},
     needRenovation: {type: Boolean, required: true},
     homeDescription: {type: String, required: true, trim: true},
+=======
+    _id: {type: Schema.Types.ObjectId, required:true},
+    firstname: {type: String, required: true, trim:true},
+    lastname: {type: String, required: true, trim:true},
+    ethnicity: {type: String, required: true},
+    gender: {type: String, required: true},
+    address: {type: String, required: true, trim: true},
+    city: {type: String, required: true, trim: true},
+    state: {type: String, required: true, trim: true},
+    zipcode: {type: String, required: true, trim: true},
+    phonenumber: {type: Number, required: true},
+    homedescription: {type: String, required: true, trim: true},
+    own: {type: Boolean, required: true},
+    rent: {type: Boolean, required: true},
+    residencystartdate: {type: Date, required: true}, //use to compute length of time in home
+    estimatedvalue: {type: Number, required: true, trim: true, min: 0},
+    ageofhome: {type: Number, required: true, trim: true, min: 0},
+    householdincome: {type: Number, required: true, trim: true, min: 0},
+    numberofadults: {type: Number, required: true, trim: true, min: 0},
+    numberofchildren: {type: Number, required: true, trim: true, min: 0},
+    bedrooms: {type: Number, required: true, trim: true, min: 0},
+    baths: {type: Number, required: true, trim: true, min: 0},
+    squarefootage: {type: Number, required: true, trim: true, min: 0},
+    recentlyrenovated: {type: Boolean, required: true},
+    needsrenovation: {type: Boolean, required: true},
+    previoushomeowner: {type: Boolean, required: true},
+    veteran: {type: String, required: true},
+    accomodations: {type: String, required: true, trim: true},
+>>>>>>> setting up edit case page
 }, { timestamps: true });
 
 const Case = mongoose.model('Case', caseSchema);
--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import HJETpic from './HJETpic.png';
 import LoginBox from './LoginBox.js';
+<<<<<<< HEAD
 import CaseForm from './CaseForm.js';
+=======
+import CaseEdit from './components/CaseEdit/CaseEdit';
+>>>>>>> setting up edit case page
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={HJETpic} />
         <LoginBox />
+<<<<<<< HEAD
       </header>
     
       {/* temporary preview until we have routes set up */}
       <div>
         <CaseForm />
       </div>
+=======
+      </header> */}
+      <CaseEdit/>
+>>>>>>> setting up edit case page
     </div>
   );
 }
