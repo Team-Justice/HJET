24a73fd09ccc9bc3f3c6284763acedf02d0d4e46
Jilene Maldonado <38514165+jilmal@users.noreply.github.com>
2020-09-03 12:35:56 -0400

    [IP] Case form (#37)
    * Add form dependencies
    
    * Implement case form skeleton
    
    * Implement form validation
    
    * Unify frontend and backend data format
    
    * Connect new case to backend
    
    Co-authored-by: Jilene Maldonado <jmaldonado32@gatech.edu>
    Co-authored-by: Sanghavi Gaddam <sgaddam32@gatech.edu>

--- backend/routes/cases.js
+++ backend/routes/cases.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 
 //returns all of the cases or specified based on parameters 
 router.route('/').get((req, res) => {
-    if (req.body.firstname && req.body.lastname && req.body.address) {
-        Case.find( {$and: [{firstname: req.body.firstname},
-            {lastname: req.body.lastname},
-            {address: {$regex: req.body.address, $options: 'i'}}]}
+    if (req.body.firstName && req.body.lastName && req.body.homeAddress) {
+        Case.find( {$and: [{firstName: req.body.firstName},
+            {lastName: req.body.lastName},
+            {homeAddress: {$regex: req.body.homeAddress, $options: 'i'}}]}
         , function(err, docs) {
             if (err) {
                 res.status(404).json(err)
@@ -15,11 +15,11 @@ router.route('/').get((req, res) => {
                 res.status(200).json(docs)
             }
         })
-    } else if (req.body.firstname && req.body.lastname) {
+    } else if (req.body.firstName && req.body.lastName) {
         console.log('reached here');
         Case.find({$and: [
-                {firstname: req.body.firstname},
-                {lastname: req.body.lastname}]}
+                {firstName: req.body.firstName},
+                {lastName: req.body.lastName}]}
             , function (err, docs) {
             if (err) {
                 res.status(404).json(err)
@@ -27,9 +27,9 @@ router.route('/').get((req, res) => {
                 res.status(200).json(docs)
             }
         })
-    } else if(req.body.firstname) {
+    } else if(req.body.firstName) {
         Case.find( {
-            firstname: req.body.firstname
+            firstName: req.body.firstName
         }
         , function(err, docs) {
             if (err) {
@@ -38,9 +38,9 @@ router.route('/').get((req, res) => {
                 res.status(200).json(docs)
             }
         })
-    } else if(req.body.lastname) {
+    } else if(req.body.lastName) {
         Case.find( {
-            lastname: req.body.lastname
+            lastName: req.body.lastName
         }
         , function(err, docs) {
             if (err) {
@@ -49,9 +49,9 @@ router.route('/').get((req, res) => {
                 res.status(200).json(docs)
             }
         })
-    } else if(req.body.address) {
+    } else if(req.body.homeAddress) {
         Case.find( {
-            address: req.body.address
+            homeAddress: req.body.homeAddress
         }
         , function(err, docs) {
             if (err) {
@@ -71,55 +71,64 @@ router.route('/').get((req, res) => {
 //add new case 
 router.route('/add').post((req, res) => {
     //const caseId = mongoose.Types.ObjectId(req.body.caseId);
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const ethinicity = req.body.ethinicity;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phoneNum = Number(req.body.phoneNum);
+    const email = req.body.email;
+    const homeAddress = req.body.homeAddress;
+    const city = req.body.city;
+    const state = req.body.state;
+    const zip = req.body.zip;
     const gender = req.body.gender;
-    const address = req.body.address;
-    const phonenumber = Number(req.body.phonenumber);
-    const homedescription = req.body.homedescription;
-    const own = Boolean(req.body.own);
-    const rent = Boolean(req.body.rent);
-    const residencystartdate = Date.parse(req.body.residencystartdate);
-    const estimatedvalue = Number(req.body.estimatedvalue);
-    const ageofhome = Number(req.body.ageofhome);
-    const householdincome = Number(req.body.householdincome);
-    const numberofresidents = {
-        adults: Number(req.body.numberofresidents.adults),
-        children: Number(req.body.numberofresidents.children),
-    }
-    const bedrooms = Number(req.body.bedrooms);
-    const baths = Number(req.body.baths);
-    const squarefootage = Number(req.body.squarefootage);
-    const recentlyrenovated = Boolean(req.body.recentlyrenovated);
-    const needsrenovation = Boolean(req.body.needsrenovation);
-    const previoushomeowner = Boolean(req.body.previoushomeowner);
-    const veteran = Boolean(req.body.veteran);
-    const accomodations = req.body.accomodations;
+    const race = req.body.race;
+    const ethnicity = req.body.ethnicity;
+    const veteran = req.body.veteran;
+    const accommodations = req.body.accommodations;
+    
+    const preHomeowner = req.body.preHomeowner;
+    const ownershipOfHome = req.body.ownershipOfHome;
+    const timeInHome = Number(req.body.timeInHome);
+    const homeValue = Number(req.body.homeValue);
+    const homeAge = Number(req.body.homeAge);
+    const householdAdults = Number(req.body.householdAdults);
+    const householdChildren = Number(req.body.householdChildren);
+    const householdIncome = Number(req.body.householdIncome);
+    const numBeds = Number(req.body.numBeds);
+    const numBaths = Number(req.body.numBaths);
+    const numSqFootage = Number(req.body.numSqFootage);
+    const recentlyRenovated = Boolean(req.body.recentlyRenovated);
+    const needRenovation = Boolean(req.body.needRenovation);
+    const homeDescription = req.body.homeDescription;
 
     const newCase = new Case({
-        firstname, 
-        lastname, 
-        ethinicity, 
+        firstName, 
+        lastName,
+        phoneNum,
+        email,
+        homeAddress, 
+        city, 
+        state, 
+        zip,
         gender, 
-        address, 
-        phonenumber, 
-        homedescription, 
-        own, 
-        rent, 
-        residencystartdate, 
-        estimatedvalue,
-        ageofhome, 
-        householdincome, 
-        numberofresidents, 
-        bedrooms, 
-        baths, 
-        squarefootage, 
-        recentlyrenovated,
-        needsrenovation, 
-        previoushomeowner, 
+        race, 
+        ethnicity,
         veteran, 
-        accomodations}); 
+        accommodations, 
+        preHomeowner,
+        ownershipOfHome,
+        timeInHome,
+        homeValue,
+        homeAge,
+        householdAdults,
+        householdChildren,
+        householdIncome, 
+        numBeds,
+        numBaths,
+        numSqFootage,
+        recentlyRenovated,
+        needRenovation,
+        homeDescription
+    }); 
 
     newCase.save()
         .then(() => res.status(201).json('new case added'))
