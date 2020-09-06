2b0088ca3c8847dad0a1b83ad0055d30144fba5a
Sanghavi Gaddam <sgaddam32@gatech.edu>
2020-09-06 10:58:37 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,26 @@ import CaseForm from './components/CaseForm/CaseForm.js';
 import CaseEdit from './components/CaseEdit/CaseEdit';
 import CaseView from './components/CaseView/CaseView';
 import NavBar from './components/NavBar/NavBar';
+import CaseSearch from './CaseSearch';
+import MainMenu from './components/MainMenu/MainMenu';
+import Cases from './components/Cases/Cases';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* <header className="App-header">
-        <img src={HJETpic} />
-        <LoginBox />
-      </header>
-      {/* temporary preview until we have routes set up */}
-      <CaseForm />
-      {/* <CaseEdit/> */}
-      <CaseView/>
+      <Router>
+        <Switch>
+          <Route path="/" exact component ={MainMenu} /> 
+          <Route path="/caseForm" exact component={CaseForm} />
+          <Route path="/caseEdit/:id" component = {CaseEdit}/>
+          <Route path="/caseView/:id" exact component={CaseView} />
+          <Route path="/caseSearch" exact component = {CaseSearch} />
+          <Route path="/cases" exact component = {Cases} />
+        </Switch>
+      </Router>
     </div>
   );
 }
