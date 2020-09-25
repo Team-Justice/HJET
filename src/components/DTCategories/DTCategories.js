992ea66997c0b61551def33e9b47c93fc96f83fa
Sangvi98 <43318897+Sangvi98@users.noreply.github.com>
2020-09-25 14:39:20 -0400

    fixing DT categories buttons (#67)

--- src/components/DTCategories/DTCategories.js
+++ src/components/DTCategories/DTCategories.js
@@ -7,22 +7,36 @@ class DTCategories extends React.Component {
     constructor(props) {
         super(props);
         this.caseID = "";
+        this.state = {
+            legacyPath: '',
+            maintainPath: '',
+            sellPath: ''
+        }
       }
 
     componentDidMount() {
         const {id} = this.props.match.params;
         this.caseID = id;
+        console.log('props values', this.props);
+        this.setState({
+            legacyPath: '/decisionTreeCategories/legacy/' + this.caseID,
+            maintainPath: '/decisionTreeCategories/maintain/' + this.caseID, 
+            sellPath: '/decisionTreeCategories/sell/' + this.caseID
+        })
     }
 
+
     render() {
+        
         return (
+            
             <div className="dtcategories-container">
                 <div className="content-container">
                     <h1>What type of decision tree would you like to fill out?</h1>
                     <div className="dt-buttons">
-                        <Link to={'/decisionTreeCategories/legacy/' + this.caseID}><Button variant="outline-secondary">Legacy Wealth Building Scheme</Button></Link>
-                        <Link to={'/decisionTreeCategories/maintain/' + this.caseID}><Button variant="outline-secondary">Maintain Current Home</Button></Link>
-                        <Link to={'/decisionTreeCategories/sell/' + this.caseID}><Button variant="outline-secondary">Sell the House</Button></Link>    
+                        <Link to={this.state.legacyPath}><Button variant="outline-secondary">Legacy Wealth Building Scheme</Button></Link>
+                        <Link to={this.state.maintainPath}><Button variant="outline-secondary">Maintain Current Home</Button></Link>
+                        <Link to={this.state.sellPath}><Button variant="outline-secondary">Sell the House</Button></Link>    
                     </div>
                 </div>
             </div>
