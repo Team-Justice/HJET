ae0fb9a24403e2163a9ee8e5b99435ea5d05fddd
Sangvi98 <43318897+Sangvi98@users.noreply.github.com>
2020-09-27 12:57:10 -0400

    removed checking json (#70)

--- src/components/MaintainHouseDecisionTree/MaintainHouseDecisionTree.js
+++ src/components/MaintainHouseDecisionTree/MaintainHouseDecisionTree.js
@@ -383,10 +383,10 @@ export default class MaintainHouseDecisionTree extends Component {
                   </Button>
 
                   {/* allows us to see state of errors in form for validation debugging */}
-                   <pre>{JSON.stringify(errors, null, 4)}</pre> 
+                   {/* <pre>{JSON.stringify(errors, null, 4)}</pre>  */}
 
                   {/* allows us to see the state of the form for debugging */}
-                   <pre>{JSON.stringify(values, null, 4)}</pre> 
+                   {/* <pre>{JSON.stringify(values, null, 4)}</pre>  */}
                 </Form>
               )}
             </Formik>
