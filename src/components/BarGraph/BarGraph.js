50b81ef65b917bdad067780d84091629fe5e29d8
Katelyn Helms <krhelms11@gatech.edu>
2020-10-01 15:24:49 -0400

    Starting Graphs Added
    graphs with fake data have been added

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import {BarChart, XAxis, YAxis, Tooltip, Legend, Bar, CartesianGrid} from 'recharts';
 
 class BarGraph extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{
+                "name": "Legacy Wealth",
+                "number": 4000,
+              },
+              {
+                "name": "Maintain Home",
+                "number": 3000,
+              },
+              {
+                "name": "Sell Home",
+                "number": 2000,
+              }]
+        }
+    }
 
     render() {
 
         return (
             <div>
                 <h1>Category Analysis</h1>
+                <BarChart width = {730} height = {250} data = {this.state.data}>
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis dataKey = "name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="number" fill="#82ca9d" />
+                </BarChart>
             </div>
         );
     }
--- src/components/TimeseriesGraph/TimeseriesGraph.js
+++ src/components/TimeseriesGraph/TimeseriesGraph.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
+import { CartesianGrid, LineChart, XAxis, YAxis, Tooltip, Legend, Line } from 'recharts';
 
 class TimeseriesGraph extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{
+                date: '07/2020', number: 200 
+            },
+            {
+                date: '08/2020', number: 150 
+
+            },
+            {
+                date: '09/2020', number: 250 
+            }]
+        }
+    }
     render() {
 
         return (
             <div>
                 <h1>Time Analysis</h1>
+                <LineChart width={500} height={300} data={this.state.data} margin= {{top:5, right:30, left:20, bottom:5,}}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey = "date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="number" stroke="#82ca9d" />
+                </LineChart>
             </div>
         );
     }
