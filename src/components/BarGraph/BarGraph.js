b908b965bda83bf3ddc5754de02540b55138fd35
Sangvi98 <43318897+Sangvi98@users.noreply.github.com>
2020-10-11 15:28:47 -0400

    updated category analysis graphs (#77)
    Co-authored-by: Sanghavi Gaddam <sgaddam32@gatech.edu>

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BarChart, XAxis, YAxis, Tooltip, Legend, Bar, CartesianGrid} from 'recharts';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
+import axios from 'axios';
 
 class BarGraph extends React.Component {
 
@@ -21,22 +22,90 @@ class BarGraph extends React.Component {
                 "name": "Sell Home",
                 "number": 2000,
               }],
-              startDate: new Date(),
-              endDate: new Date()
+            startDate: new Date(),
+            endDate: new Date(),
+            legacyDecisionTrees: [],
+            maintainDecisionTrees: [],
+            sellDecisionTrees: []
         }
 
+        this.updateData = this.updateData.bind(this);
+        this.filterData = this.filterData.bind(this);
+
     }
 
-    handleStartDateChange(event) {
+    async componentDidMount() {
+        const legacyDT = await axios.get('http://localhost:5000/legacy-wealth-building/cases');
         this.setState({
+            legacyDecisionTrees: legacyDT.data
+        });
+
+        const maintainDT = await axios.get('http://localhost:5000/maintain-current-home/cases');
+        this.setState({
+            maintainDecisionTrees: maintainDT.data
+        });
+
+        const sellDT = await axios.get('http://localhost:5000/sell-House/cases');
+        this.setState({
+            sellDecisionTrees: sellDT.data
+        });
+
+    }
+
+    async handleStartDateChange(event) {
+        await this.setState({
             startDate: event
         });
+        this.updateData();
     }
 
-    handleEndDateChange(event) {
-        this.setState({
+    async handleEndDateChange(event) {
+        await this.setState({
             endDate: event
         });
+        this.updateData();
+    }
+
+    async updateData() {
+        
+        var filteredLegacyDT = this.filterData(this.state.legacyDecisionTrees);
+        var filteredMaintainDT = this.filterData(this.state.maintainDecisionTrees);
+        var filteredSellDT = this.filterData(this.state.sellDecisionTrees);
+        
+        console.log(filteredLegacyDT);
+
+        var newData = [];
+        newData.push({
+            "name": "Legacy Wealth",
+            "number": filteredLegacyDT.length,
+        });
+        newData.push({
+            "name": "Maintain Home",
+            "number": filteredMaintainDT.length,
+        });
+        newData.push({
+            "name": "Sell Home",
+            "number": filteredSellDT.length,
+        })
+
+        this.setState({
+            data: newData
+        })
+
+    }
+
+    filterData(dataArray) {
+        const sortedDataArray = dataArray.sort((a, b) => b.createdAt - a.createdAt);
+        const filteredDataArray = sortedDataArray.filter(decisionTree => {
+            var decisionTreeDate = decisionTree.createdAt;
+            var date = decisionTreeDate.split("-");
+            var dateYear = date[0];
+            var dateMonth = date[1];
+            var dateDay = date[2].split("T")[0];
+            var decisionTreeNewDate = new Date(dateYear, dateMonth - 1, dateDay)
+            return decisionTreeNewDate >= this.state.startDate && decisionTreeNewDate <= this.state.endDate;
+        });
+        return filteredDataArray;
     }
 
     render() {
